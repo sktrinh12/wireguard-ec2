ce6da63b12e3bae6d9603338025ad4223fc480b2
sktrinh12 <sktrinh12@gmail.com>
2024-10-02 18:53:35 -0500

    add profile flag

--- config/device_scripts/iam_cleanup.sh
+++ config/device_scripts/iam_cleanup.sh
@@ -5,14 +5,15 @@ ROLE_NAME=$1
 INSTANCE_PROFILE_NAME=$2
 POLICY_NAME=$3
 REGION=$4
+PROFILE=${5:-default}
 
 # Step 1: Detach and delete the policy from the role
-POLICY_ARNS=$(aws iam list-attached-role-policies --role-name "$ROLE_NAME" --query "AttachedPolicies[*].PolicyArn" --output text --region "$REGION")
+POLICY_ARNS=$(aws iam list-attached-role-policies --role-name "$ROLE_NAME" --query "AttachedPolicies[*].PolicyArn" --output text --region "$REGION" --profile "$PROFILE")
 
 if [ -n "$POLICY_ARNS" ]; then
     for POLICY_ARN in $POLICY_ARNS; do
-        aws iam detach-role-policy --role-name "$ROLE_NAME" --policy-arn "$POLICY_ARN" --region "$REGION"
-        aws iam delete-policy --policy-arn "$POLICY_ARN" --region "$REGION"
+        aws iam detach-role-policy --role-name "$ROLE_NAME" --policy-arn "$POLICY_ARN" --region "$REGION" --profile "$PROFILE"
+        aws iam delete-policy --policy-arn "$POLICY_ARN" --region "$REGION" --profile "$PROFILE"
         echo "Deleted policy $POLICY_ARN"
     done
 else
@@ -20,13 +21,13 @@ else
 fi
 
 # Step 2: Delete the inline policy (if exists)
-aws iam delete-role-policy --role-name "$ROLE_NAME" --policy-name "$POLICY_NAME" --region "$REGION" || echo "No inline policy found."
+aws iam delete-role-policy --role-name "$ROLE_NAME" --policy-name "$POLICY_NAME" --region "$REGION" --profile "$PROFILE" || echo "No inline policy found."
 
 # Step 3: Remove the role from the instance profile
-aws iam remove-role-from-instance-profile --instance-profile-name "$INSTANCE_PROFILE_NAME" --role-name "$ROLE_NAME" --region "$REGION" || echo "Role not attached to instance profile."
+aws iam remove-role-from-instance-profile --instance-profile-name "$INSTANCE_PROFILE_NAME" --role-name "$ROLE_NAME" --region "$REGION" --profile "$PROFILE"|| echo "Role not attached to instance profile."
 
 # Step 4: Delete the instance profile
-aws iam delete-instance-profile --instance-profile-name "$INSTANCE_PROFILE_NAME" --region "$REGION" || echo "Instance profile not found."
+aws iam delete-instance-profile --instance-profile-name "$INSTANCE_PROFILE_NAME" --region "$REGION" --profile "$PROFILE" || echo "Instance profile not found."
 
 # Step 5: Delete the IAM role
-aws iam delete-role --role-name "$ROLE_NAME" --region "$REGION" || echo "Role not found."
+aws iam delete-role --role-name "$ROLE_NAME" --region "$REGION" --profile "$PROFILE" || echo "Role not found."
