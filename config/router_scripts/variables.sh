398a3fc342fe3f5083049f3170a0a3bed8977fa6
sktrinh12 <sktrinh12@gmail.com>
2024-10-02 18:53:21 -0500

    dynamic bucket name and region

--- config/router_scripts/variables.sh
+++ config/router_scripts/variables.sh
@@ -12,15 +12,17 @@ PEER_PORT=51820
 POLICY_NAME="tf-exec-policy"
 PEER_NAME="vpn"
 ROLE_NAME="tf-exec-role"
-BUCKET_NAME="tf-ec2-state"
-BUCKET_REGION="us-east-2"
 KEY_PREFIX="wireguard"
 GPG_PASSPHRASE=$(gpg --batch --yes --decrypt /mnt/creds/input.gpg)
 
 if [ "$1" == "1" ]; then
     GPG_FILE="/mnt/creds/aws_chom.gpg"
+    BUCKET_NAME="tf-ec2-state-chom"
+    BUCKET_REGION="us-east-1"
 else
     GPG_FILE="/mnt/creds/aws.gpg"
+    BUCKET_NAME="tf-ec2-state"
+    BUCKET_REGION="us-east-2"
 fi
 
 eval $(gpg --batch --yes --passphrase "$GPG_PASSPHRASE" --decrypt $GPG_FILE)
