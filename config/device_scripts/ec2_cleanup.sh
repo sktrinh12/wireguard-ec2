a75bc31c1e0b351cb37f09817504fcb9b58280cc
sktrinh12 <sktrinh12@gmail.com>
2024-10-03 12:01:00 -0500

    add profile flag

--- config/device_scripts/ec2_cleanup.sh
+++ config/device_scripts/ec2_cleanup.sh
@@ -3,27 +3,28 @@
 # Inputs
 REGION=$1
 NAME_TAG=$2
+PROFILE=$3
 
 # Step 1: Get Instance IDs
-INSTANCE_IDS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$NAME_TAG" --query "Reservations[*].Instances[*].InstanceId" --output text --region "$REGION")
+INSTANCE_IDS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$NAME_TAG" --query "Reservations[*].Instances[*].InstanceId" --output text --region "$REGION" --profile "$PROFILE")
 
 # Delete Security Groups
-SECURITY_GROUP_IDS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query "Reservations[*].Instances[*].SecurityGroups[*].GroupId" --output text --region "$REGION")
+SECURITY_GROUP_IDS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query "Reservations[*].Instances[*].SecurityGroups[*].GroupId" --output text --region "$REGION" --profile "$PROFILE")
 
 if [ -n "$SECURITY_GROUP_IDS" ]; then
   for SG_ID in $SECURITY_GROUP_IDS; do
-      aws ec2 delete-security-group --group-id "$SG_ID" --region "$REGION" > /dev/null 2>&1 || echo "Security group $SG_ID could not be deleted."
+      aws ec2 delete-security-group --group-id "$SG_ID" --region "$REGION" --profile "$PROFILE" > /dev/null 2>&1 || echo "Security group $SG_ID could not be deleted."
   done
 else
     echo "No dangling security groups found to terminate."
 fi
 
 # Delete Subnets
-SUBNET_IDS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query "Reservations[*].Instances[*].SubnetId" --output text --region "$REGION")
+SUBNET_IDS=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query "Reservations[*].Instances[*].SubnetId" --output text --region "$REGION" --profile "$PROFILE")
 
 if [ -n "$SUBNET_IDS" ]; then
   for SUBNET_ID in $SUBNET_IDS; do
-      aws ec2 delete-subnet --subnet-id "$SUBNET_ID" --region "$REGION" > /dev/null 2>&1 || echo "Subnet $SUBNET_ID could not be deleted."
+      aws ec2 delete-subnet --subnet-id "$SUBNET_ID" --region "$REGION" --profile "$PROFILE" > /dev/null 2>&1 || echo "Subnet $SUBNET_ID could not be deleted."
   done
 else
     echo "No dangling subnet ids found to terminate."
@@ -31,9 +32,9 @@ fi
 
 # Delete EC2 Instance
 if [ -n "$INSTANCE_IDS" ]; then
-    aws ec2 terminate-instances --instance-ids $INSTANCE_IDS --region "$REGION" > /dev/null 2>&1
+    aws ec2 terminate-instances --instance-ids $INSTANCE_IDS --region "$REGION" --profile "$PROFILE"> /dev/null 2>&1
 
-    aws ec2 wait instance-terminated --instance-ids $INSTANCE_IDS --region "$REGION"
+    aws ec2 wait instance-terminated --instance-ids $INSTANCE_IDS --region "$REGION" --profile "$PROFILE"
     echo "Terminated instances: $INSTANCE_IDS"
 else
     echo "No EC2 instances found to terminate."
