7f3d7ebe989274105f8d558931aefaa1e3ba0d92
sktrinh12 <strkinh12@gmail.com>
2025-01-23 10:12:08 -0500

    add jq for user tfexecutor to set ssm

--- config/router_scripts/user_data.sh
+++ config/router_scripts/user_data.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y gnupg software-properties-common git wget
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com \$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update -y
-sudo apt install -y terraform
+sudo apt install -y terraform jq
 cd /home/ubuntu
 git clone {{GIT_REPO}}
 cd {{NAME}}
