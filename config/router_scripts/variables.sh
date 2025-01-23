8b1a6d95628b377a67cd89e5cd06bbfab79db0fa
sktrinh12 <strkinh12@gmail.com>
2025-01-23 10:33:18 -0500

    add public ip and change ip address of interface

--- config/router_scripts/variables.sh
+++ config/router_scripts/variables.sh
@@ -6,7 +6,7 @@ NAME="wireguard-ec2"
 GIT_REPO="https://github.com/sktrinh12/${NAME}.git"
 INSTANCE_PROFILE_NAME="tf-exec-instance-profile"
 INSTANCE_TYPE="t2.micro"
-IP_ADDRESS="10.0.0.2/24"
+IP_ADDRESS="10.131.54.2/24,fd11:5ee:bad:c0de::a83:3602/64"
 PEER_PORT=51820
 POLICY_NAME="tf-exec-policy"
 PEER_NAME="vpn"
@@ -14,6 +14,7 @@ ROLE_NAME="tf-exec-role"
 KEY_PREFIX="wireguard"
 CREDS_PATH="/root/creds"
 EIP_ALLOC_ID="eipalloc-0f3204f9f1538ed2f"
+PUBLIC_IP="34.193.198.229"
 GPG_PASSPHRASE=$(cat ${CREDS_PATH}/input.txt)
 
 echo "profile: $1"
