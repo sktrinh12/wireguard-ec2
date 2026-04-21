91673df70a7cfb37665eb1cb15a8436daf4b296a
sktrinh12 <strkinh12@gmail.com>
2026-04-21 00:49:59 -0400

    add the second aws provider resource with alias and different bucket region

--- providers.tf
+++ providers.tf
@@ -1,3 +1,10 @@
 provider "aws" {
-  region = var.region
+  region  = var.region
+  profile = var.aws_profile
+}
+
+provider "aws" {
+  alias   = "bucket"
+  region  = var.bucket_region
+  profile = var.aws_profile
 }
