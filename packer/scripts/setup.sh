368c509f5ab9d38a778abd6dc1434dc6cf773a29
sktrinh12 <strkinh12@gmail.com>
2026-04-20 20:28:11 -0400

    add exit out error set declaration, use of apt-get instead of apt

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -ex
 export DEBIAN_FRONTEND=noninteractive
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
@@ -7,9 +7,10 @@ echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo deb
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
 echo "Installing awscli"
-sudo apt update
-sudo apt install -yq software-properties-common
-sudo apt install -yq wireguard iptables-persistent curl zip jq
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-get update
+sudo apt-get install -y wireguard iptables-persistent curl zip jq
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
